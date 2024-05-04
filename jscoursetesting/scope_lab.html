34b836cee4a887e1564b6d9c3fe0160c4afe1753
osamayub <usama.ayub1@ucp.edu.pk>
2024-05-04 14:46:31 +0500

    scopes and variables in javascript

---
+++ jscoursetesting/scope_lab.html
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scoped Variables</title>
+</head>
+<body>
+    <h1>Scope Lab for Var, Let and Const</h1>
+    </h1>
+    <script src="./scope_lab.js"></script>
+</body>
+</html>
---
+++ jscoursetesting/scope_lab.js
@@ -0,0 +1,17 @@
+
+//global scope
+
+var globalVar="I'm a global Variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+  // Block scope
+  var blockVar = "I'm a block-scoped var";
+  let blockLet = "I'm a block-scoped let";
+  const blockConst = "I'm a block-scoped const";
+}
+
+
+//functional scope
