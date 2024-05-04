94a0758f43c3d616008024fa664345bd9053fc66
osamayub <usama.ayub1@ucp.edu.pk>
2024-05-04 14:52:56 +0500

    functional scope concept

--- jscoursetesting/scope_lab.js
+++ jscoursetesting/scope_lab.js
@@ -12,6 +12,15 @@ const globalConst = "I'm a global constant";
   let blockLet = "I'm a block-scoped let";
   const blockConst = "I'm a block-scoped const";
 }
-
-
 //functional scope
+
+function show() {
+  var functionVar = "I'm a block-scoped var";
+  let functionLet = "I'm a block-scoped let";
+  const functionConst = "I'm a block-scoped const";
+}
+show();
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); //Throws ReferenceError
+
