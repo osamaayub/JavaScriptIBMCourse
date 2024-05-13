2cf23ac17355cc2365dee3784fb51076de7d6592
osamayub <usama.ayub1@ucp.edu.pk>
2024-05-13 22:32:45 +0500

    grocery amount calculated

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,13 @@ function calculateArea() {
   let area = lenght * width;
   document.getElementById('result').innerHTML = `The area of rectangle is:${area}`;
 }
+function groceryTracker(amount) {
+  let grocery1 = parseFloat(document.getElementById('grocery1').value);
+  let grocery2 = parseFloat(document.getElementById('grocery2').value);
+  let grocery3 = parseFloat(document.getElementById('grocery3').value);
+  let totalAmount = grocery1 + grocery2 + grocery3;
+
+  document.getElementById('result').innerHTML = `The total Amount is:${totalAmount}`;
+
+
+}
